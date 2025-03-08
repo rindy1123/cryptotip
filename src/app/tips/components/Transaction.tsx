4054ca496cd0026ed563f0a746f924f4b5322162
rindy1123 <genryusinbaou@gmail.com>
2025-03-08 14:29:33 +0900

    fix: value validation

--- src/app/tips/components/Transaction.tsx
+++ src/app/tips/components/Transaction.tsx
@@ -4,12 +4,14 @@ import { useSearchParams } from "next/navigation";
 import { Arrow } from "@/app/components/Arrow";
 import { PrimaryButton } from "@/app/components/PrimaryButton";
 import { useAppKit, useAppKitAccount } from "@reown/appkit/react";
+import { useState } from "react";
 
 export const Transaction = () => {
   const params = useSearchParams();
   const to = params.get("addr");
   const { open } = useAppKit();
   const { isConnected } = useAppKitAccount();
+  const [failed, setFailed] = useState(false);
   const { data: hash, sendTransaction } = useSendTransaction();
 
   // TODO: Fallback pages
@@ -21,12 +23,17 @@ export const Transaction = () => {
     return <div>Invalid Address</div>;
   }
 
-  async function submit(e: React.FormEvent<HTMLFormElement>, to: Address) {
+  function submit(e: React.FormEvent<HTMLFormElement>, to: Address) {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
-    // TODO: Validate value
     const value = formData.get("value") as string;
-    sendTransaction({ to, value: parseEther(value) });
+    try {
+      sendTransaction({ to, value: parseEther(value) });
+      setFailed(false);
+    } catch (error) {
+      console.error(error);
+      setFailed(true);
+    }
   }
 
   return (
@@ -40,6 +47,9 @@ export const Transaction = () => {
           <input
             name="value"
             placeholder="0.05"
+            type="number"
+            min="0.00001"
+            step="0.00001"
             required
             className="w-2/3 text-xl rounded-xl bg-gray-800 p-2 border-gray-600 border focus:outline-none focus:ring-0"
           />
@@ -50,6 +60,15 @@ export const Transaction = () => {
           <h2 className="lg:text-2xl md:text-2xl text-xl">{to}</h2>
           <h3>⚠️Please double-check the address before sending</h3>
         </div>
+        {failed && (
+          <div
+            className="bg-red-900 border-l-4 border-red-500 p-4 mb-4"
+            role="alert"
+          >
+            <p className="font-bold">Error</p>
+            <p>Failed to send a tranaction</p>
+          </div>
+        )}
         {isConnected ? (
           <PrimaryButton type="submit" text="Send" />
         ) : (
